47c396e7b9e886b74305a8ccc3637a57484f6f62
Yang Renyi <jianghurainxwy@gmail.com>
2023-10-29 02:21:53 +0100

    plot

--- crates/dies/src/path_planning/mod.rs
+++ crates/dies/src/path_planning/mod.rs
@@ -1,7 +1,10 @@
 extern crate nalgebra as na;
+use std::error::Error;
 use na::Vector2;
 extern crate float_cmp;
 use float_cmp::approx_eq;
+extern crate plotters;
+use plotters::prelude::*;
 
 /// The number of points in the path
 const N_POINTS: usize = 5;
@@ -25,6 +28,7 @@ impl Obstacle {
         Self { position, radius, velocity: Vector2::new(0.0, 0.0) }
     }
 }
+
 
 pub fn compute_path(
     start: &mut Vector2<f32>,
@@ -74,6 +78,33 @@ pub fn compute_path(
     path
 }
 
+pub fn plot_environment(start: &mut Vector2<f32>, goal: &Vector2<f32>, obstacles: &[Obstacle]) -> Result<(), Box<dyn Error>> {
+    let root_area = BitMapBackend::new("plot.png", (900, 600)).into_drawing_area();
+    root_area.fill(&WHITE)?;
+
+    let mut chart = ChartBuilder::on(&root_area)
+        .build_cartesian_2d(0.0..6.0, 0.0..9.0)?;
+
+    chart.draw_series(vec![
+        Circle::new((start.x as f64, start.y as f64), 3, &RED),
+        Circle::new((goal.x as f64, goal.y as f64), 3, &GREEN),
+    ])?;
+
+    for obstacle in obstacles {
+        println!("Obstacle: {:?}", obstacle.position);
+        chart.draw_series(vec![
+            Circle::new((obstacle.position.x as f64, obstacle.position.y as f64), (obstacle.radius*100.0) as f64, &BLACK)
+        ])?;
+    }
+
+    let path = compute_path(start, &goal, obstacles, None);
+    let path_f64: Vec<(f64, f64)> = path.iter().map(|point| (point.x as f64, point.y as f64)).collect();
+    chart.draw_series(LineSeries::new(path_f64, &BLUE))?;
+
+    Ok(())
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -117,5 +148,16 @@ mod tests {
         }
     }
 
+    /*
+    #[test]
+    fn test_with_plot()  -> Result<(), Box<dyn std::error::Error>>{
+        let mut start = Vector2::new(1.0, 1.0);
+        let goal = Vector2::new(3.0, 5.0);
+        let obstacles = [Obstacle::new(Vector2::new(2.0, 2.5), 0.08,
+                                       Vector2::new(1.0, 0.0))];
+
+        plot_environment(&mut start, &goal, &obstacles)?;
+        Ok(())
+    }*/
 
 }
