a80056d1a9cb7bb8f65396b2d97771aee850e03a
mablin7 <magyarb0201@gmail.com>
2024-02-20 20:16:35 +0100

    feat: delay measure scripts

--- strategy/test-strat/src/dies_test_strat/__main__.py
+++ strategy/test-strat/src/dies_test_strat/__main__.py
@@ -12,13 +12,35 @@ print("Starting test-strat")
 if __name__ == "__main__":
     bridge = Bridge()
 
+    start_time = None
+    start_pos = None
+    to_save = []
+    rid = 5
     while True:
         msg = bridge.recv()
         if isinstance(msg, Term):
             break
-        elif isinstance(msg, World):
-            print("Received world")
-            print(msg)
-            time.sleep(1)
-    bridge.close()
+        if not msg:
+            continue
+        player = next((p for p in msg.own_players if p.id == rid), None)
+        if not player:
+            # print("Player not found")
+            continue
+        pos = np.array([player.position[0], player.position[1]])
+
+        if start_time is None:
+            start_time = time.time()
+            start_pos = pos
+        else:
+            dist = np.linalg.norm(pos - start_pos)
+            elapsed = time.time() - start_time
+            to_save.append((elapsed, dist))
+            if dist > 2:
+                print("Distance travelled:", dist)
+                print("Time taken:", elapsed)
+                break
+
+        bridge.send(PlayerCmd(rid, 10, 0))
+        time.sleep(10e-4)
+    np.save("test-strat.npy", to_save)
     print("Exiting test-strat")
