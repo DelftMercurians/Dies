7de922614a02b86687934093799ef810e5a6cd76
Yohan Le Gars <yohan.legars@gmail.com>
2024-02-27 19:26:43 +0100

    Add start of MPC impl

--- crates/dies-controller/src/lib.rs
+++ crates/dies-controller/src/lib.rs
@@ -15,6 +15,35 @@ struct MPCParams{
     // add other params here
 }
 
+fn cost_function(u: &[f64], params: &MPCParams) -> f64 {
+    // the cost function must be implemented here based on params and u.
+}
 
 
-impl ModelPredictiveControl {}
+fn cost_gradient(u: &[f64], params: &MPCParams, grad: &mut [f64]) {
+    // calculate grad based on params and u.
+}
+
+pub struct MPC {
+    params: MPCParams,
+    optimizer: PANOCOptimizer<>,
+}
+
+impl MPC {
+
+    pub fn new(params: MPCParams) -> Self{
+        let bounds = NoConstraints::new();
+        let problem =  Problem::new(&bounds, move |u,grad| cost_gradient(u, &params, grad), move |u| cost_function(u, &params));
+        let optimizer = PANOCOptimizer::new(problem, SolverOptions::default());
+        Self { params, optimizer}
+
+    }
+
+
+    pub fn solve(&mut self, initial_guess: &[f64]) -> Result<Vec<f64>, SolverError> {
+        let mut u = initial_guess.to_vec(); // Copy initial guess to the optimization variable
+        let res = self.optimizer.solve(&mut u)?;
+        Ok(u) // Return the optimized control input
+    }
+
+}
