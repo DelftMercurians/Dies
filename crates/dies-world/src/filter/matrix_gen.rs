903cbb24dd8f06969d504745f37c3b44e7eb1859
Renyi Yang <R.Yang-7@student.tudelft.nl>
2024-03-02 18:13:06 +0100

    feat: matrix generator

---
+++ crates/dies-world/src/filter/matrix_gen.rs
@@ -0,0 +1,134 @@
+use std::ops::Mul;
+use nalgebra as na;
+use na::{DimName, OMatrix, U2,};
+use na::{Const, DefaultAllocator, SMatrix};
+use na::allocator::Allocator;
+
+type U4 = Const<4>;
+fn block_diag(
+    a: &OMatrix<f32, U2, U2>,
+) -> OMatrix<f32, U4, U4>
+{
+    let mut result = OMatrix::<f32, U4, U4>::zeros();
+    result.fixed_slice_mut::<2, 2>(0, 0).copy_from(a);
+    result.fixed_slice_mut::<2, 2>(2, 2).copy_from(a);
+    result
+}
+
+pub trait MatrixCreator<D>
+    where
+        D: DimName,
+        DefaultAllocator: Allocator<f32, D, D>,
+{
+    fn create_matrix(&self, delta_t: f32) -> OMatrix<f32, D, D>;
+}
+
+struct Piecewise1stOrder;
+
+// 2x2 measurement noise matrix assuming acceleration is different in
+// different duration
+impl MatrixCreator<U2> for Piecewise1stOrder {
+    fn create_matrix(&self, delta_t: f32) -> OMatrix<f32, U2, U2> {
+        OMatrix::<f32, U2, U2>::new(
+            delta_t.powi(4)/4.0, delta_t.powi(3)/2.0,
+            delta_t.powi(3)/2.0, delta_t.powi(2),
+        )
+    }
+}
+
+//Piecewise1stOrder but 2D
+impl MatrixCreator<U4> for Piecewise1stOrder {
+    fn create_matrix(&self, delta_t: f32) -> OMatrix<f32,U4,U4> {
+        let m = OMatrix::<f32, U2, U2>::new(
+            delta_t.powi(4)/4.0, delta_t.powi(3)/2.0,
+            delta_t.powi(3)/2.0, delta_t.powi(2),
+        );
+        block_diag(&m)
+    }
+}
+
+struct WhiteNoise1stOrder;
+
+// 2x2 measurement noise matrix assuming acceleration does not change
+impl MatrixCreator<U2> for WhiteNoise1stOrder {
+    fn create_matrix(&self, delta_t: f32) -> OMatrix<f32, U2, U2> {
+        OMatrix::<f32, U2, U2>::new(
+            delta_t.powi(3)/3.0,  delta_t.powi(2)/2.0,
+            delta_t.powi(2)/2.0, delta_t,
+        )
+    }
+}
+
+//WhiteNoise1stOrder but 2D
+impl MatrixCreator<U4> for WhiteNoise1stOrder {
+    fn create_matrix(&self, delta_t: f32) -> OMatrix<f32,U4,U4> {
+        let m = OMatrix::<f32, U2, U2>::new(
+            delta_t.powi(3)/3.0,  delta_t.powi(2)/2.0,
+            delta_t.powi(2)/2.0, delta_t,
+        );
+        block_diag(&m)
+    }
+}
+
+struct ULMotionModel;
+
+// 2x2 transition matrix assuming constant speed
+impl MatrixCreator<U2> for ULMotionModel {
+    fn create_matrix(&self, delta_t: f32) -> OMatrix<f32, U2, U2> {
+        OMatrix::<f32, U2, U2>::new(
+            1.0, delta_t,
+            0.0, 1.0,
+        )
+    }
+}
+
+//ULMotionModel but 2D
+impl MatrixCreator<U4> for ULMotionModel {
+    fn create_matrix(&self, delta_t: f32) -> OMatrix<f32,U4,U4> {
+        let m = OMatrix::<f32, U2, U2>::new(
+            1.0, delta_t,
+            0.0, 1.0,
+        );
+        block_diag(&m)
+    }
+}
+
+
+
+// observation transformation matrix 1d [x, vx] -> [x]
+fn state_to_measurement() -> SMatrix<f32, 1, 2> {
+    SMatrix::<f32, 1, 2>::new(1.0, 0.0)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_white_noise_1st_order_u2() {
+        let wn = WhiteNoise1stOrder;
+        let delta_t = 2.0_f32;
+        let result_matrix:OMatrix<f32,U2,U2> = wn.create_matrix(delta_t);
+        let expected_matrix = OMatrix::<f32, U2, U2>::new(
+            8.0/3.0, 4.0/2.0,
+            4.0/2.0, 2.0,
+        );
+
+        assert!((result_matrix - expected_matrix).norm() < 1e-6);
+    }
+
+    #[test]
+    fn test_white_noise_1st_order_u4() {
+        let wn = WhiteNoise1stOrder;
+        let delta_t = 1.0_f32;
+        let result_matrix:OMatrix<f32,U4,U4> = wn.create_matrix(delta_t);
+        let expected_matrix = OMatrix::<f32, U4, U4>::new(
+            1.0/3.0, 1.0/2.0, 0.0, 0.0,
+            1.0/2.0, 1.0, 0.0, 0.0,
+            0.0, 0.0, 1.0/3.0, 1.0/2.0,
+            0.0, 0.0, 1.0/2.0, 1.0,
+        );
+
+        assert_eq!(result_matrix, expected_matrix);
+    }
+}
+
