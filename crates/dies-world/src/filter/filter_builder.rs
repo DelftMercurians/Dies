5a626ea253d1397eaa291f189ec9d11bf3f6c8c5
Renyi Yang <R.Yang-7@student.tudelft.nl>
2024-03-02 22:24:36 +0100

    builder
    feat: buidler

---
+++ crates/dies-world/src/filter/filter_builder.rs
@@ -0,0 +1,41 @@
+use nalgebra as na;
+use na::{ OMatrix, U2,};
+use na::OVector;
+use na::U4;
+use crate::filter::kalman::Kalman;
+use crate::filter::matrix_gen::{MatrixCreator, ULMotionModel, WhiteNoise1stOrder};
+
+struct KalmanBuilder {
+    init_std: f64,
+    measurement_std: f64,
+    unit_transition_std: f64
+}
+
+impl KalmanBuilder {
+    pub fn new(init_std: f64, measurement_std: f64, unit_transition_std: f64) -> Self {
+        KalmanBuilder {
+            init_std,
+            measurement_std,
+            unit_transition_std
+        }
+    }
+
+    pub fn build2D(&self, init_pos:OVector<f64, U4>, int_time: f64) -> Kalman<U2,U4> {
+        let std = self.unit_transition_std;
+        let t = int_time;
+        let A: dyn MatrixCreator<U4> = ULMotionModel;
+        let H: OMatrix<f64, U2, U4> = OMatrix::<f64, U2, U4>::new(
+            1.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 1.0, 0.0,
+        );
+        let Q: dyn MatrixCreator<U4> = WhiteNoise1stOrder;
+        let R: OMatrix<f64, U2, U2> = OMatrix::<f64, U2, U2>::new(
+            self.measurement_std, 0.0,
+            0.0, self.measurement_std,
+        );
+        let P: OMatrix<f64, U4, U4> = OMatrix::<f64, U4, U4>::identity() * self.init_std;
+        let x: OVector<f64, U4> = init_pos;
+
+        Kalman::new(std, t, Box::new(A), H, Box::new(Q), R, P, x)
+    }
+}
--- crates/dies-world/src/filter/kalman.rs
+++ crates/dies-world/src/filter/kalman.rs
@@ -2,7 +2,7 @@ use nalgebra as na;
 use na::{DimName, OMatrix, U2,};
 use na::{Const, DefaultAllocator, SMatrix};
 use na::allocator::Allocator;
-use nalgebra::OVector;
+use na::OVector;
 use crate::filter::matrix_gen::MatrixCreator;
 
 // OS: Observation Space, SS: State Space
@@ -18,28 +18,28 @@ where
     // Transition matrix
     A: dyn MatrixCreator<SS>,
     // Transformation (observation) matrix
-    H: OMatrix<f32, OS, SS>,
+    H: OMatrix<f64, OS, SS>,
     // Process noise covariance matrix
     Q: dyn MatrixCreator<SS>,
     // Measurement noise covariance matrix
-    R: OMatrix<f32, OS, OS>,
+    R: OMatrix<f64, OS, OS>,
     // Error covariance matrix
-    P: OMatrix<f32, SS, SS>,
+    P: OMatrix<f64, SS, SS>,
     // State vector
-    x: OVector<f32, SS>,
+    x: OVector<f64, SS>,
 }
 
 impl <OS,SS> Kalman<OS,SS>
 where
     OS: DimName,
     SS: DimName,
-    DefaultAllocator: Allocator<f32, OS>,
-    DefaultAllocator: Allocator<f32, SS>,
-    DefaultAllocator: Allocator<f32, SS, SS>,
-    DefaultAllocator: Allocator<f32, OS, SS>,
-    DefaultAllocator: Allocator<f32, SS, OS>,
-    DefaultAllocator: Allocator<f32, SS>,
-    DefaultAllocator: Allocator<f32, OS, OS>,
+    DefaultAllocator: Allocator<f64, OS>,
+    DefaultAllocator: Allocator<f64, SS>,
+    DefaultAllocator: Allocator<f64, SS, SS>,
+    DefaultAllocator: Allocator<f64, OS, SS>,
+    DefaultAllocator: Allocator<f64, SS, OS>,
+    DefaultAllocator: Allocator<f64, SS>,
+    DefaultAllocator: Allocator<f64, OS, OS>,
 {
 
 
@@ -47,11 +47,11 @@ where
         std: f64,
         t: f64,
         A: Box<dyn MatrixCreator<SS>>,
-        H: OMatrix<f32, OS, SS>,
+        H: OMatrix<f64, OS, SS>,
         Q: Box<dyn MatrixCreator<SS>>,
-        R: OMatrix<f32, OS, OS>,
-        P: OMatrix<f32, SS, SS>,
-        x: OVector<f32, SS>,
+        R: OMatrix<f64, OS, OS>,
+        P: OMatrix<f64, SS, SS>,
+        x: OVector<f64, SS>,
     ) -> Self {
         Kalman {
             std,
@@ -66,13 +66,13 @@ where
     }
 
     //predict a future state, this doesn't change the internal state of the filter
-    pub fn predict(&mut self, newt: f64) {
+    pub fn predict(&mut self, newt: f64) -> OVector<f64, SS> {
         &self.A.create_matrix(newt - self.t) * &self.x
     }
 
     //update the state of the filter based on a new observation and a new time
     //returns None if the packet is dropped
-    pub fn update(&mut self, z: OVector<f32, OS>, newt: f64) -> Option<OVector<f32, SS>>{
+    pub fn update(&mut self, z: OVector<f64, OS>, newt: f64) -> Option<OVector<f64, SS>>{
         let dt = newt - self.t;
         if dt < 0.0 {
             return None;
@@ -80,7 +80,7 @@ where
         let A = self.A.create_matrix(dt);
         let Q = self.Q.create_matrix(dt);
         let x = &A * &self.x;
-        let P = &A * &self.P * &A.transpose() + &Q;
+        let P = &A * &self.P * &A.transpose() + &Q*self.std.powi(2);
         let r = z - &self.H * &x;
         let S = &self.H * &P * &self.H.transpose() + &self.R;
         let K = &P * &self.H.transpose() * S.try_inverse().unwrap();
