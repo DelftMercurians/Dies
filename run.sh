1e56dbfcf421ecef95a0ef2aca0822c56948a513
mablin7 <magyarb0201@gmail.com>
2024-06-13 00:16:27 +0200

    fix: script

--- run.sh
+++ run.sh
@@ -4,13 +4,13 @@
 trap ctrl_c INT
 
 
-set TIMESTAMP = $(date -u +"%Y-%m-%dT%H:%M:%SZ")
-set LOG_FILE_PATH = "/home/mercury/.local/share/dies/dies-$TIMESTAMP.log"
+TIMESTAMP=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+LOG_FILE_PATH="/home/mercury/.local/share/dies/dies-$TIMESTAMP.log"
 
 function pull_logs() {
-    echo "** Pulling logs..."
+    echo "** Pulling logs (from $LOG_FILE_PATH)..."
     mkdir -p ./logs
-    scp mercury@merucryvision:$LOG_FILE_PATH ./logs/
+    scp mercury@mercuryvision:$LOG_FILE_PATH ./logs/
 }
 
 has_pressed_ctrl_c=false
@@ -22,7 +22,7 @@ function ctrl_c() {
     has_pressed_ctrl_c=true
     
     echo "** Trapped CTRL-C"
-    ssh mercury@merucryvision "killall dies-cli"
+    ssh mercury@mercuryvision "killall dies-cli"
     pull_logs
     exit 1
 }
@@ -37,10 +37,10 @@ if [ ! "$(ls -A ./crates/dies-webui/static)" ] || [ "$1" == "--web-build" ]; the
 fi
 
 # Sync files to the remote server
-rsync -avz --delete --exclude-from='.gitignore' --exclude-from='.rsyncignore' --exclude .git  . mercury@merucryvision:Code/dies/
+rsync -avz --delete --exclude-from='.gitignore' --exclude-from='.rsyncignore' --exclude .git  . mercury@mercuryvision:Code/dies/
 
 # Run the program on the remote server
-ssh -L 5555:localhost:5555 mercury@merucryvision "cd Code/dies && /home/mercury/.cargo/bin/cargo run -- $@"
+ssh -L 5555:localhost:5555 mercury@mercuryvision "cd Code/dies && /home/mercury/.cargo/bin/cargo run -- --log-file $LOG_FILE_PATH $@"
 
 # Pull logs
 pull_logs
