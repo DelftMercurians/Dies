88c7925e9aa064b647e96c1e320eea53074392c4
ivrolan <ivanlpzbrc01@gmail.com>
2024-07-08 22:43:12 +0200

    orca: problem is feasible only if the velocity is not perpendicular to the obstacle
    Signed-off-by: ivrolan <ivanlpzbrc01@gmail.com>

--- crates/dies-executor/src/roles/test_role.rs
+++ crates/dies-executor/src/roles/test_role.rs
@@ -34,12 +34,12 @@ impl Role for TestRole {
         
         // CONSTANTS OF GEOMETRY
 
-        let target = Vector2::new(2000.0, 2000.0);
+        let target = Vector2::new(2000.0, 100.0);
         let size = 250;
-        let a = Vector2::new(-size as f64, -size as f64);
-        let b = Vector2::new(-size as f64, size as f64);
-        let c = Vector2::new(size as f64, size as f64);
-        let d = Vector2::new(size as f64, -size as f64);
+        let a = Vector2::new( - size as f64, -size as f64);
+        let b = Vector2::new(  - size as f64, size as f64);
+        let c = Vector2::new(size as f64,  size as f64);
+        let d = Vector2::new(size as f64,  -size as f64);
 
         // DRAWING
         dbg_draw!(("p{}.target", ctx.player.id), cross, target);
@@ -49,67 +49,10 @@ impl Role for TestRole {
         dbg_draw!(("p{}.obs3", ctx.player.id), line, c, d);
         dbg_draw!(("p{}.obs4", ctx.player.id), line, d, a);
 
-        // match invoke_skill!(ctx, GoToPositionSkill::new(target)) {
-        //     SkillResult::Continue(mut input) => {
-        //         let desired_vel = match input.velocity {
-        //             Velocity::Global(v) => Vec2::new(v.x as f32, v.y as f32),
-        //             Velocity::Local(_) => return input,
-        //         };
-
-        //         // PlayerControlInput::new()
-        //         let obstacles = vec![
-        //             Obstacle::Closed {
-        //                 vertices: vec![
-        //                     Vec2::new(a.x as f32, a.y as f32),
-        //                     Vec2::new(b.x as f32, b.y as f32),
-        //                     Vec2::new(c.x as f32, c.y as f32),
-        //                     Vec2::new(d.x as f32, d.y as f32),
-        //                 ],
-        //             },
-        //             // Add more obstacles here.
-        //         ];
-        //         let nearby_obstacles = obstacles
-        //             .iter()
-        //             .map(|obstacle| Cow::Borrowed(obstacle))
-        //             .collect::<Vec<_>>();
-
-        //         let time_horizon = 3.0;
-        //         let obstacle_time_horizon = 1.0;
-
-        //         let avoidance_velocity : Vec2 = self.agent.compute_avoiding_velocity(
-        //             // Neighbors - other players
-        //             &vec![],
-        //             // Obstacles
-        //             nearby_obstacles.as_slice(),
-        //             desired_vel,
-        //             100.0,
-        //             ctx.world.dt as f32,
-        //             &AvoidanceOptions {
-        //                 obstacle_margin: 0.1,
-        //                 time_horizon,
-        //                 obstacle_time_horizon,
-        //             },
-        //         );
-
-        //         input.velocity = Velocity::global(Vector2::new(
-        //             avoidance_velocity.x as f64,
-        //             avoidance_velocity.y as f64,
-        //         ));
-        //         dies_core::debug_line(format!("p{}.orca_velocity", ctx.player.id), ctx.player.position, ctx.player.position + Vector2::new(avoidance_velocity.x as f64, avoidance_velocity.y as f64), dies_core::DebugColor::Purple);
-        //         dies_core::debug_value(format!("p{}.orca_vel_x", ctx.player.id), avoidance_velocity.x as f64);
-        //         dies_core::debug_value(format!("p{}.orca_vel_y", ctx.player.id), avoidance_velocity.y as f64);
-        //         input
-        //     }
-        //     SkillResult::Done => PlayerControlInput::new(),
-        // }
-
-        // let target = Vector2::new(1000.0, 1000.0);
-        // dbg_draw!(("p{}.target", ctx.player.id), cross, target);
-        // skill!(ctx, GoToPositionSkill::new(target));
-        // // skill!(ctx, GoToPositionSkill::new(target2));
+      
         let mut input = PlayerControlInput::new();
         let preferred_vel = target - ctx.player.position;
-        let max_speed = 100000.0;
+        let max_speed = 1000.0;
         input.velocity = Velocity::global(Vector2::new(preferred_vel.x , preferred_vel.y).normalize() * max_speed);
 
         self.agent.position = Vec2::new(ctx.player.position.x as f32, ctx.player.position.y as f32);
@@ -124,9 +67,10 @@ impl Role for TestRole {
                                 Cow::Owned(Obstacle::Closed{
                                     vertices: vec![
                                     Vec2::new(a.x as f32, a.y as f32),
-                                    Vec2::new(b.x as f32, b.y as f32),
-                                    Vec2::new(c.x as f32, c.y as f32),
                                     Vec2::new(d.x as f32, d.y as f32),
+                                    Vec2::new(c.x as f32, c.y as f32),
+                                    Vec2::new(b.x as f32, b.y as f32),
+                                   
                                     ],
                                 }),
                                 // Add more obstacles here.
@@ -146,10 +90,10 @@ impl Role for TestRole {
                         // Obstacles
                         &nearby_obstacles,
                         desired_vel,
-                        1000.0,
+                        max_speed as f32,
                         ctx.world.dt as f32,
                         &AvoidanceOptions {
-                            obstacle_margin: 1.,
+                            obstacle_margin: 200.,
                             time_horizon,
                             obstacle_time_horizon,
                         },
