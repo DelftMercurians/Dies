223b634e755f59854323ef5299cc0e392516480f
mablin7 <magyarb0201@gmail.com>
2024-07-08 18:48:02 +0200

    feat: wait skill

--- crates/dies-executor/src/roles/skills/mod.rs
+++ crates/dies-executor/src/roles/skills/mod.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use dies_core::Vector2;
 
 use crate::PlayerControlInput;
@@ -31,5 +33,44 @@ impl Skill for GoToPositionSkill {
         let mut input = PlayerControlInput::new();
         input.with_position(self.target);
         SkillResult::Continue(input)
+    }
+}
+
+/// A skill that just waits for a certain amount of time
+pub struct WaitSkill {
+    amount: f64,
+    until: Option<f64>,
+}
+
+impl WaitSkill {
+    /// Creates a new `WaitSkill` that waits for the given amount of time (starting
+    /// from the next frame)
+    pub fn new(amount: Duration) -> Self {
+        Self {
+            amount: amount.as_secs_f64(),
+            until: None,
+        }
+    }
+
+    /// Creates a new `WaitSkill` that waits for the given amount of time in seconds
+    /// (starting from the next frame)
+    pub fn new_secs_f64(amount: f64) -> Self {
+        Self {
+            amount,
+            until: None,
+        }
+    }
+}
+
+impl Skill for WaitSkill {
+    fn update(&mut self, ctx: SkillCtx<'_>) -> SkillResult {
+        let until = *self
+            .until
+            .get_or_insert_with(|| ctx.world.t_received + self.amount);
+        if ctx.world.t_received >= until {
+            SkillResult::Done
+        } else {
+            SkillResult::Continue(PlayerControlInput::new())
+        }
     }
 }
