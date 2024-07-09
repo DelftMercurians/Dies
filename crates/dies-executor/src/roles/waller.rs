3e2d8ff3d6960003d26ce666d09ae6a4db7b8f15
mablin7 <magyarb0201@gmail.com>
2024-07-09 16:12:43 +0200

    fix: use player control input

--- crates/dies-executor/src/roles/waller.rs
+++ crates/dies-executor/src/roles/waller.rs
@@ -1,3 +1,4 @@
+use dies_core::Angle;
 use dies_core::BallData;
 use dies_core::PlayerData;
 use nalgebra::Vector2;
@@ -7,9 +8,8 @@ use crate::{
     skill, PlayerControlInput,
 };
 
-
 pub struct Waller {
-    pub offset: f64,
+    offset: f64,
 }
 use super::RoleCtx;
 
@@ -18,9 +18,9 @@ impl Waller {
         Self { offset }
     }
 
-    fn find_intersection(&self, player_data: &PlayerData, ball: &BallData) -> Vector2<f64> {
+    fn find_intersection(&self, _player_data: &PlayerData, ball: &BallData) -> Vector2<f64> {
         let goal_center = Vector2::new(6117.0, -129.0); // Center of the goal area
-        let ball_pos = Vector2::new(ball.position.x, ball.position.y);
+        let ball_pos = ball.position.xy();
 
         // Compute the direction vector from the ball to the goal center
         let direction: Vector2<f64> = goal_center - ball_pos;
@@ -31,7 +31,7 @@ impl Waller {
         // Points representing the boundary of the goalkeeper area
         let area_top = 1400.0; // top boundary y-coordinate
         let area_bottom = -1400.0; // bottom boundary y-coordinate
-        let area_right = 4630.0;  // right boundary x-coordinate
+        let area_right = 4630.0; // right boundary x-coordinate
 
         // Intersect with the top boundary
         if direction.y != 0.0 {
@@ -61,80 +61,78 @@ impl Waller {
         }
 
         // Default fallback to ball position (should not happen in normal cases)
-        // CHANGE THIS TO MIDDLE OF THE GOAL 
+        // CHANGE THIS TO MIDDLE OF THE GOAL
         //println!("Falling back to ball position");
-        return Vector2::new(area_right, ball_pos.y)
+        return Vector2::new(area_right, ball_pos.y);
 
-    // fn find_intersection(&self, player_data: &PlayerData, ball: &BallData) -> Vector2<f64> {
-    //     let goal_center = Vector2::new(6117.0, -129.0); // Center of the goal area
-    //     let ball_pos = Vector2::new(ball.position.x, ball.position.y);
+        // fn find_intersection(&self, player_data: &PlayerData, ball: &BallData) -> Vector2<f64> {
+        //     let goal_center = Vector2::new(6117.0, -129.0); // Center of the goal area
+        //     let ball_pos = Vector2::new(ball.position.x, ball.position.y);
 
-    //     // Compute the direction vector from the ball to the goal center
-    //     let direction: Vector2<f64> = goal_center - ball_pos;
-    //     // Normalize the direction vector
-    //     let direction = direction.normalize();
-    //     println!("Direction: {:?}", direction);
+        //     // Compute the direction vector from the ball to the goal center
+        //     let direction: Vector2<f64> = goal_center - ball_pos;
+        //     // Normalize the direction vector
+        //     let direction = direction.normalize();
+        //     println!("Direction: {:?}", direction);
 
-    //     // Points representing the boundary of the goalkeeper area
-    //     let area_top = 1400.0; // top boundary y-coordinate
-    //     let area_bottom = -1400.0; // bottom boundary y-coordinate
-    //     let area_right = 4630.0;  // right boundary x-coordinate
+        //     // Points representing the boundary of the goalkeeper area
+        //     let area_top = 1400.0; // top boundary y-coordinate
+        //     let area_bottom = -1400.0; // bottom boundary y-coordinate
+        //     let area_right = 4630.0;  // right boundary x-coordinate
 
-    //     // Intersect with the top boundary
-    //     if direction.y != 0.0 {
+        //     // Intersect with the top boundary
+        //     if direction.y != 0.0 {
 
-    //         let t_top = (area_top - ball_pos.y) / direction.y;
-    //         let x_top = ball_pos.x + t_top * direction.x;
+        //         let t_top = (area_top - ball_pos.y) / direction.y;
+        //         let x_top = ball_pos.x + t_top * direction.x;
 
-    //         let t_bottom = (area_bottom - ball_pos.y) / direction.y;
-    //         let x_bottom = ball_pos.x + t_bottom * direction.x;
+        //         let t_bottom = (area_bottom - ball_pos.y) / direction.y;
+        //         let x_bottom = ball_pos.x + t_bottom * direction.x;
 
-    //         if player_data.position.y < area_top && player_data.position.y > area_bottom {
-    //             if x_bottom.abs() >= area_right && x_bottom.abs() <= 6125.0 {
-    //                 let bottom_corner = Vector2::new(area_right, area_bottom);
-    //                 if player_data.position != bottom_corner {
-    //                     return bottom_corner;
-    //                 } else {
-    //                     return Vector2::new(x_bottom, area_bottom);
-    //                 }
-    //             }
-    //             if x_top.abs() >= area_right && x_top.abs() <= 6125.0 {
-    //                 // First go to the top corner
-    //                 let top_corner = Vector2::new(area_right, area_top);
-    //                 println!("Moving to top corner: {:?}", top_corner);
-    //                 if player_data.position != top_corner {
-    //                     return top_corner;
-    //                 } else {
-    //                     return Vector2::new(x_top, area_top);
-    //                 }
-    //             }
-    //         }
+        //         if player_data.position.y < area_top && player_data.position.y > area_bottom {
+        //             if x_bottom.abs() >= area_right && x_bottom.abs() <= 6125.0 {
+        //                 let bottom_corner = Vector2::new(area_right, area_bottom);
+        //                 if player_data.position != bottom_corner {
+        //                     return bottom_corner;
+        //                 } else {
+        //                     return Vector2::new(x_bottom, area_bottom);
+        //                 }
+        //             }
+        //             if x_top.abs() >= area_right && x_top.abs() <= 6125.0 {
+        //                 // First go to the top corner
+        //                 let top_corner = Vector2::new(area_right, area_top);
+        //                 println!("Moving to top corner: {:?}", top_corner);
+        //                 if player_data.position != top_corner {
+        //                     return top_corner;
+        //                 } else {
+        //                     return Vector2::new(x_top, area_top);
+        //                 }
+        //             }
+        //         }
 
-    //         // Normal intersection checks
-    //         if x_top.abs() >= area_right && x_top.abs() <= 6125.0 {
-    //             return Vector2::new(x_top, area_top);
-    //         }
+        //         // Normal intersection checks
+        //         if x_top.abs() >= area_right && x_top.abs() <= 6125.0 {
+        //             return Vector2::new(x_top, area_top);
+        //         }
 
-    //         if x_bottom.abs() >= area_right && x_bottom.abs() <= 6125.0 {
-    //             return Vector2::new(x_bottom, area_bottom);
-    //         }
-    //         }
+        //         if x_bottom.abs() >= area_right && x_bottom.abs() <= 6125.0 {
+        //             return Vector2::new(x_bottom, area_bottom);
+        //         }
+        //         }
 
-    //         if direction.x != 0.0 {
-    //             let t = (area_right - ball_pos.x) / direction.x;
-    //             let y = ball_pos.y + t * direction.y;
-    //             if y.abs() <= area_top {
-    //                 return Vector2::new(area_right, y);
-    //             }
-    //         }
+        //         if direction.x != 0.0 {
+        //             let t = (area_right - ball_pos.x) / direction.x;
+        //             let y = ball_pos.y + t * direction.y;
+        //             if y.abs() <= area_top {
+        //                 return Vector2::new(area_right, y);
+        //             }
+        //         }
 
         // Default fallback to ball position (should not happen in normal cases)
-        // CHANGE THIS TO MIDDLE OF THE GOAL 
+        // CHANGE THIS TO MIDDLE OF THE GOAL
         //println!("Falling back to ball position");
         // return Vector2::new(area_right, ball_pos.y)
     }
-
-
 
     fn angle_to_ball(&self, player_data: &PlayerData, ball: &BallData) -> f64 {
         // Angle needed to face the passer (using arc tangent)
@@ -150,8 +148,11 @@ impl Role for Waller {
     fn update(&mut self, ctx: RoleCtx<'_>) -> PlayerControlInput {
         let ball_pos = ctx.world.ball.as_ref().unwrap();
         let target_pos = self.find_intersection(ctx.player, ball_pos);
-        skill!(ctx, GoToPositionSkill::new(target_pos));
-        // input.with_orientation(target_angle);
-        PlayerControlInput::new()
+        let mut input = PlayerControlInput::new();
+        input.with_position(target_pos);
+        input.with_yaw(Angle::from_radians(
+            self.angle_to_ball(ctx.player, ball_pos),
+        ));
+        input
     }
 }
