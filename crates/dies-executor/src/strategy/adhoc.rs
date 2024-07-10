23203489f5dcf35bd117e3bb1131fb9aefab24ab
mablin7 <magyarb0201@gmail.com>
2024-07-10 23:51:31 +0200

    misc: reorganize

---
+++ crates/dies-executor/src/strategy/adhoc.rs
@@ -0,0 +1,80 @@
+use super::Strategy;
+use super::StrategyCtx;
+use crate::roles::Role;
+use dies_core::PlayerId;
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
+
+/// A strategy that assigns roles to players based on a predefined list. Useful for
+/// testing.
+///
+/// This strategy will reassign roles every time it is entered.
+///
+/// # Example
+///
+/// ```ignore
+/// let mut strategy = AdHocStrategy::new();
+///
+/// // This role will be assigned to player 0 if there is a player with that ID,
+/// // otherwise nothing will happen.
+/// strategy.add_role_with_id(0, Box::new(Attacker::new()));
+///
+/// // This role will be assigned to the first available player.
+/// strategy.add_role(Box::new(Defender::new()));
+/// ```
+pub struct AdHocStrategy {
+    pub(crate) roles: HashMap<PlayerId, Box<dyn Role>>,
+    pub(crate) unassigned_roles: Vec<Box<dyn Role>>,
+}
+
+impl Default for AdHocStrategy {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl AdHocStrategy {
+    pub fn new() -> Self {
+        AdHocStrategy {
+            roles: HashMap::new(),
+            unassigned_roles: Vec::new(),
+        }
+    }
+
+    /// Add a role to a specific player. If the player does not exist, the role will
+    /// never be updated so effectively it will not be assigned.
+    pub fn add_role_with_id(&mut self, id: PlayerId, role: Box<dyn Role>) {
+        self.roles.insert(id, role);
+    }
+
+    /// Add a role to the list of unassigned roles. On the next update, the role will be
+    /// to the first available player.
+    pub fn add_role(&mut self, role: Box<dyn Role>) {
+        self.unassigned_roles.push(role);
+    }
+}
+
+impl Strategy for AdHocStrategy {
+    fn on_enter(&mut self, _ctx: StrategyCtx) {
+        // Clear roles
+        self.roles.clear();
+    }
+
+    fn update(&mut self, ctx: StrategyCtx) {
+        // Assign roles to players
+        for player_data in ctx.world.own_players.iter() {
+            if let Entry::Vacant(e) = self.roles.entry(player_data.id) {
+                if let Some(role) = self.unassigned_roles.pop() {
+                    e.insert(role);
+                }
+            }
+        }
+        if !self.unassigned_roles.is_empty() {
+            log::warn!("Not enough players to assign all roles");
+        }
+    }
+
+    fn get_roles(&mut self) -> &mut HashMap<PlayerId, Box<dyn Role>> {
+        &mut self.roles
+    }
+}
