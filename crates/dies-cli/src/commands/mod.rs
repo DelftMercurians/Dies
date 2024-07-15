b9001e38be9d50728e1729ff913bb59b220663fb
Delft-Mercurians <roboteamtud@gmail.com>
2024-07-15 17:01:19 +0200

    fix: move args

--- crates/dies-cli/src/commands/mod.rs
+++ crates/dies-cli/src/commands/mod.rs
@@ -1,2 +1,77 @@
+use anyhow::{bail, Result};
+use dies_basestation_client::list_serial_ports;
+use start_ui::MainArgs;
+use tokio::io::{AsyncBufReadExt, BufReader};
+
+use crate::cli::SerialPort;
+
 pub mod convert_logs;
 pub mod start_ui;
+pub mod test_radio;
+
+/// Selects a serial port based on the CLI arguments. This function may prompt the user
+/// to choose a port if multiple ports are available and the `serial_port` argument is
+/// set to "auto".
+async fn select_serial_port(args: &MainArgs) -> Result<String> {
+    let ports = list_serial_ports();
+    let port = match &args.serial_port {
+        SerialPort::Disabled => None,
+        SerialPort::Auto => {
+            if ports.is_empty() {
+                log::warn!("No serial ports found, disabling serial");
+                None
+            } else if ports.len() == 1 {
+                log::info!("Connecting to serial port {}", ports[0]);
+                Some(ports[0].clone())
+            } else {
+                println!("Available ports:");
+                for (idx, port) in ports.iter().enumerate() {
+                    println!("{}: {}", idx, port);
+                }
+
+                // Let user choose port
+                loop {
+                    println!("Enter port number:");
+                    if let Some(input) = read_line().await {
+                        if let Ok(port_idx) = input.trim().parse::<usize>() {
+                            if port_idx < ports.len() {
+                                break Some(ports[port_idx].clone());
+                            }
+                        } else {
+                            println!("Invalid port number");
+                        }
+                    } else {
+                        println!("Invalid port number");
+                    }
+                }
+            }
+        }
+        SerialPort::Port(port) => {
+            if !ports.contains(port) {
+                println!(
+                    "Available ports:\n{}",
+                    ports
+                        .iter()
+                        .map(|p| format!("  - {}", p))
+                        .collect::<Vec<_>>()
+                        .join("\n")
+                );
+                bail!("Port {} not found", port);
+            }
+            Some(port.clone())
+        }
+    };
+
+    port.ok_or(anyhow::anyhow!("Serial port not found"))
+}
+
+async fn read_line() -> Option<String> {
+    let reader = BufReader::new(tokio::io::stdin());
+    reader
+        .lines()
+        .next_line()
+        .await
+        .ok()
+        .flatten()
+        .map(|s| s.trim().to_owned())
+}
--- crates/dies-cli/src/commands/start_ui.rs
+++ crates/dies-cli/src/commands/start_ui.rs
@@ -1,11 +1,108 @@
 use anyhow::{bail, Result};
-use std::str::FromStr;
+use clap::Parser;
+use dies_basestation_client::{BasestationClientConfig, BasestationHandle};
+use dies_ssl_client::VisionClientConfig;
+use dies_webui::{UiConfig, UiEnvironment};
+use std::{net::SocketAddr, path::PathBuf, str::FromStr};
 
 use dies_logger::AsyncProtobufLogger;
 use log::{LevelFilter, Log};
 use tokio::sync::broadcast;
 
-use crate::cli::MainArgs;
+use crate::cli::{BasestationProtocolVersion, SerialPort, VisionType};
+
+use super::select_serial_port;
+
+#[derive(Debug, Parser)]
+#[command(name = "dies-cli")]
+pub struct MainArgs {
+    #[clap(long, short = 'f', default_value = "dies-settings.json")]
+    pub settings_file: PathBuf,
+
+    #[clap(long, default_value = "auto", default_missing_value = "auto")]
+    pub serial_port: SerialPort,
+
+    #[clap(long, default_value = "v1")]
+    pub protocol: BasestationProtocolVersion,
+
+    #[clap(long, default_value = "5555")]
+    pub webui_port: u16,
+
+    #[clap(long, default_value = "false")]
+    pub disable_python: bool,
+
+    #[clap(long, default_value = "")]
+    pub robot_ids: String,
+
+    #[clap(long, default_value = "udp")]
+    pub vision: VisionType,
+
+    #[clap(long, default_value = "224.5.23.2:10006")]
+    pub vision_addr: SocketAddr,
+
+    #[clap(long, default_value = "info")]
+    pub log_level: String,
+
+    #[clap(long, default_value = "logs")]
+    pub log_directory: String,
+}
+
+impl MainArgs {
+    /// Converts the CLI arguments into a `UiConfig` object that can be used to start the web UI.
+    pub async fn into_ui(self) -> Result<UiConfig> {
+        let environment = match (self.serial_config().await, self.vision_config()) {
+            (Some(bs_config), Some(ssl_config)) => UiEnvironment::WithLive {
+                bs_handle: BasestationHandle::spawn(bs_config)?,
+                ssl_config,
+            },
+            _ => UiEnvironment::SimulationOnly,
+        };
+
+        Ok(UiConfig {
+            settings_file: self.settings_file,
+            environment,
+            port: self.webui_port,
+        })
+    }
+
+    /// Configures the serial client based on the CLI arguments. This function may prompt the user
+    /// to choose a port if multiple ports are available and the `serial_port` argument is set to "auto".
+    ///
+    /// If there is an issue selecting a serial port, an error message will be logged and `None` will be returned.
+    pub async fn serial_config(&self) -> Option<BasestationClientConfig> {
+        select_serial_port(self)
+            .await
+            .map_err(|err| log::warn!("Failed to setup serial: {}", err))
+            .ok()
+            .map(|port| {
+                let mut config = BasestationClientConfig::new(port, self.protocol.into());
+                config.set_robot_id_map_from_string(&self.robot_ids);
+                config
+            })
+    }
+
+    /// Configures the vision client based on the CLI arguments.
+    pub fn vision_config(&self) -> Option<VisionClientConfig> {
+        match self.vision {
+            VisionType::None => None,
+            VisionType::Tcp => Some(VisionClientConfig::Tcp {
+                host: self.vision_addr.ip().to_string(),
+                port: self.vision_addr.port(),
+            }),
+            VisionType::Udp => Some(VisionClientConfig::Udp {
+                host: self.vision_addr.ip().to_string(),
+                port: self.vision_addr.port(),
+            }),
+        }
+    }
+
+    /// Returns the path to the log directory, making sure it exists. Defaults to "logs" in the current directory.
+    pub async fn ensure_log_dir_path(&self) -> Result<PathBuf> {
+        let path = PathBuf::from(&self.log_directory);
+        tokio::fs::create_dir_all(&path).await?;
+        Ok(path)
+    }
+}
 
 pub async fn start_ui(args: MainArgs) -> Result<()> {
     // Set up logging
