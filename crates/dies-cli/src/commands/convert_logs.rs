85988495973ff0f875e0192150044bfefb232add
mablin7 <magyarb0201@gmail.com>
2024-07-20 01:14:50 +0200

    fix: convert log memory

--- crates/dies-cli/src/commands/convert_logs.rs
+++ crates/dies-cli/src/commands/convert_logs.rs
@@ -1,6 +1,7 @@
-use std::path::Path;
-
+use anyhow::Context;
 use dies_logger::LogFile;
+use std::io::{BufWriter, Write};
+use std::path::Path;
 
 fn msg_to_json(msg: &dies_logger::TimestampedMessage) -> serde_json::Value {
     let mut map = serde_json::Map::new();
@@ -12,9 +13,28 @@ fn msg_to_json(msg: &dies_logger::TimestampedMessage) -> serde_json::Value {
         dies_logger::LogMessage::DiesData(data) => {
             map.insert("data".to_string(), serde_json::to_value(data).unwrap());
         }
-        dies_logger::LogMessage::DiesLog(_) => {}
-        dies_logger::LogMessage::Vision(_) => {}
-        dies_logger::LogMessage::Referee(_) => {}
+        dies_logger::LogMessage::DiesLog(log) => {
+            map.insert(
+                "log".to_string(),
+                log.message.clone().unwrap_or_default().into(),
+            );
+        }
+        dies_logger::LogMessage::Vision(msg) => {
+            map.insert(
+                "vision".to_string(),
+                protobuf_json_mapping::print_to_string(msg)
+                    .unwrap_or_default()
+                    .into(),
+            );
+        }
+        dies_logger::LogMessage::Referee(msg) => {
+            map.insert(
+                "referee".to_string(),
+                protobuf_json_mapping::print_to_string(msg)
+                    .unwrap_or_default()
+                    .into(),
+            );
+        }
         dies_logger::LogMessage::Bytes(_) => {}
     }
     serde_json::Value::Object(map)
@@ -22,20 +42,36 @@ fn msg_to_json(msg: &dies_logger::TimestampedMessage) -> serde_json::Value {
 
 pub fn convert_log(input: &Path, output: &Path) -> anyhow::Result<()> {
     println!("Converting log file {} to JSON", input.display());
-
     if !input.exists() || !input.is_file() {
         return Err(anyhow::anyhow!("Input file does not exist"));
     }
 
     let logs = LogFile::open(input)?;
+    let out_file = std::fs::File::create(output)?;
+    let mut writer = BufWriter::new(out_file);
 
-    let out = std::fs::File::create(output)?;
-    let messages = logs.messages().iter().map(msg_to_json).collect::<Vec<_>>();
+    writer.write_all(b"[")?;
 
-    serde_json::to_writer_pretty(out, &messages)?;
+    let mut first = true;
+    let mut count = 0;
 
-    println!("Converted {} messages", messages.len());
+    for msg in logs.messages() {
+        if !first {
+            writer.write_all(b",")?;
+        } else {
+            first = false;
+        }
+
+        let json = msg_to_json(msg);
+        serde_json::to_writer(&mut writer, &json).context("Failed to serialize message")?;
+
+        count += 1;
+    }
+
+    writer.write_all(b"]")?;
+    writer.flush()?;
+
+    println!("Converted {} messages", count);
     println!("Output written to {}", output.display());
-
     Ok(())
 }
