19249f74342bbd0ee3d836741d9690ac52d02e7f
Roman Knyazhitskiy <git@knyaz.tech>
2025-06-30 23:26:01 +0000

    disable log compiles

--- mpc_jax/mpc_jax/__init__.py
+++ mpc_jax/mpc_jax/__init__.py
@@ -14,10 +14,12 @@ import jax
 jax.config.update("jax_compilation_cache_dir", ".jax_cache")
 jax.config.update("jax_persistent_cache_min_entry_size_bytes", -1)
 jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
-jax.config.update("jax_persistent_cache_enable_xla_caches", "xla_gpu_per_fusion_autotune_cache_dir")
+jax.config.update(
+    "jax_persistent_cache_enable_xla_caches", "xla_gpu_per_fusion_autotune_cache_dir"
+)
 
 # jax.config.update("jax_disable_jit", True)
-jax.config.update("jax_log_compiles", True)
+# jax.config.update("jax_log_compiles", True)
 jax.config.update("jax_debug_nans", True)
 
 from .main import solve_mpc, solve_mpc_tbwrap
