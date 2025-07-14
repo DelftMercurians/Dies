b1eb553ae21873985dff6b2c509e94cdb69a4330
mablin7 <magyarb0201@gmail.com>
2025-07-14 20:22:55 -0300

    kick?

--- crates/dies-executor/src/skills/kick.rs
+++ crates/dies-executor/src/skills/kick.rs
@@ -1,5 +1,3 @@
-use std::time::Instant;
-
 use dies_core::SysStatus;
 
 use super::{SkillCtx, SkillProgress};
@@ -7,16 +5,12 @@ use crate::{KickerControlInput, PlayerControlInput};
 
 #[derive(Clone)]
 pub struct Kick {
-    has_kicked: usize,
-    timer: Option<Instant>,
+    has_kicked: bool,
 }
 
 impl Kick {
     pub fn new() -> Self {
-        Self {
-            has_kicked: 1,
-            timer: None,
-        }
+        Self { has_kicked: false }
     }
 }
 
@@ -25,43 +19,14 @@ impl Kick {
         let mut input = PlayerControlInput::new();
         input.with_dribbling(1.0);
 
-        if self.has_kicked == 0 {
-            let timer = self.timer.get_or_insert(Instant::now());
-            if timer.elapsed().as_secs_f64() > 0.1 {
-                if ctx
-                    .world
-                    .ball
-                    .as_ref()
-                    .map(|b| b.detected && (b.position.xy() - ctx.player.position).norm() > 200.0)
-                    .unwrap_or(false)
-                {
-                    return SkillProgress::success();
-                } else {
-                    return SkillProgress::failure();
-                }
-            } else {
-                return SkillProgress::Continue(input);
-            }
-        }
-
         let ready = match ctx.player.kicker_status.as_ref() {
             Some(SysStatus::Ready) => true,
             _ => false,
         };
 
         if ready {
-            if !ctx.player.breakbeam_ball_detected
-                && (ctx
-                    .world
-                    .ball
-                    .as_ref()
-                    .map(|b| b.detected && ((b.position.xy() - ctx.player.position).norm() < 400.0))
-                    .unwrap_or(false))
-            {
-                return SkillProgress::failure();
-            }
             input.with_kicker(KickerControlInput::Kick { force: 1.0 });
-            self.has_kicked -= 1;
+            self.has_kicked = true;
             SkillProgress::Continue(input)
         } else {
             input.kicker = KickerControlInput::Arm;
