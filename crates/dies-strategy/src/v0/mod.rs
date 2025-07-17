fe4a9ac98f20dc2e53a93eef22ab7a29d35d01d4
mablin7 <magyarb0201@gmail.com>
2025-07-17 19:38:25 -0300

    .

--- crates/dies-strategy/src/v0/mod.rs
+++ crates/dies-strategy/src/v0/mod.rs
@@ -193,7 +193,7 @@ pub fn v0_defence(game: &mut GameContext) {
         game.add_role("striker_1")
             .max(1)
             .score(score_striker)
-            // .exclude(|s| s.has_any_handicap(&[Handicap::NoKicker, Handicap::NoDribbler]))
+            .exclude(|s| s.has_any_handicap(&[Handicap::NoKicker, Handicap::NoDribbler]))
             .behavior(|s| striker::build_striker_tree(s));
     } else {
         game.add_role("harasser_1")
@@ -212,7 +212,7 @@ pub fn v0_defence(game: &mut GameContext) {
         game.add_role("striker_2")
             .max(1)
             .score(score_striker)
-            // .exclude(|s| s.has_any_handicap(&[Handicap::NoKicker, Handicap::NoDribbler]))
+            .exclude(|s| s.has_any_handicap(&[Handicap::NoKicker, Handicap::NoDribbler]))
             .behavior(|s| striker::build_striker_tree(s));
     } else {
         game.add_role("harasser_2")
@@ -231,7 +231,7 @@ pub fn v0_defence(game: &mut GameContext) {
     game.add_role("striker_3")
         // .max(1)
         .score(score_striker)
-        // .exclude(|s| s.has_any_handicap(&[Handicap::NoKicker, Handicap::NoDribbler]))
+        .exclude(|s| s.has_any_handicap(&[Handicap::NoKicker, Handicap::NoDribbler]))
         .behavior(|s| striker::build_striker_tree(s));
     // }
 }
